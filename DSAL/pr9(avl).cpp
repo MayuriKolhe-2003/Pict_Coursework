c01de2ea94a525b3a14c7538cec5c357a5e4a1c6
Mayuri Kolhe <77161474+MayuriKolhe-2003@users.noreply.github.com>
2023-06-05 19:58:55 +0530

    added avl del

--- DSAL/pr9(avl).cpp
+++ DSAL/pr9(avl).cpp
@@ -1,245 +1,240 @@
-//============================================================================
-// Name        : pr9(avl).cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
+///avl insertion and deletion
 
 #include <iostream>
-#include <algorithm>
 #include <queue>
-#include <bits/stdc++.h>
+
 using namespace std;
 
-class Node {
-	int key;
-	string value;
-	int height=1;
-	Node *left;
-	Node *right;
-public:
-	Node() {
-		key = -1;
-		value = "";
-		height = 1;
-		left = right = nullptr;
-	}
-	Node(int k,string v){
-		key = k;
-		value = v;
-		height = 1;
-		left = right = nullptr;
-	}
-	string getValue(){
-		return value;
-	}
-	friend class AVLTree;
+class node{
+    int key;
+    string value;
+    int height = 1;
+    node *left;
+    node *right;
+    
+    public:
+    node(){
+        key = 0;
+        value = "";
+        height = 1;
+        left = nullptr;
+        right = nullptr;
+    }
+    node(int k,string v){
+        key = k ;
+        value = v;
+        height = 1;
+        left = nullptr ;
+        right = nullptr;
+    }
+    friend class avl;
 };
 
-class AVLTree {
-	Node *root = nullptr;
-public:
-	int getHeight(Node *t) {
-		if (t == nullptr) {
-			return 0;
-		}
-		return t->height;
-	}
-	int getBalanceFactor(Node *t) {
-		if (t == nullptr) {
-			return 0;
-		}
-		return getHeight(t->left) - getHeight(t->right);
-	}
-	void updateHeight(Node *t) {
-	    int hl = getHeight(t->left);
-	    int hr = getHeight(t->right);
-		t->height = 1 + max(hl , hr);
-	}
-
-	Node* LLRotation(Node *t) {
-		Node *newRoot = t->left;
-		t->left = newRoot->right;
-		newRoot->right = t;
-
-		// Updating the heights of node and newRoot
-		// to in-turn update balance factors of these nodes
-		updateHeight(t);
-		updateHeight(newRoot);
-
-		cout << "[Performed LL Rotation]" << endl;
-		return newRoot;
-	}
-
-	Node* RRRotation(Node *t) {
-		Node *newRoot = t->right;
-		t->right = newRoot->left;
-		newRoot->left = t;
-
-		// Updating the heights of node and newRoot
-		// to in-turn update balance factors of these nodes
-		updateHeight(t);
-		updateHeight(newRoot);
-
-		cout << "[Performed RR Rotation]" << endl;
-		return newRoot;
-	}
-
-	Node* RLRotation(Node *t) {
-		t->right = LLRotation(t->right);
-		return RRRotation(t);
-
-	}
-
-	Node* LRRotation(Node *t) {
-		t->left = RRRotation(t->left);
-		return LLRotation(t);
-	}
-
-	Node* balance(Node *node) {
-		if (getBalanceFactor(node) == 2) {
-			// Left subtree has a greater height
-			// Possible rotations: LL, LR
-		if(getBalanceFactor(node->left)  < 0 ) {
-			// For left child, right subtree has greater height. Hence, use LR rotation
-			node = LRRotation(node);
-		} else {
-			// For left child, left subtree has greater height. Hence, use LL rotation
-			node = LLRotation(node);
-		}
-	} else if (getBalanceFactor(node) == -2) {
-		// Right subtree has a greater height
-		// Possible rotations: RR, RL
-	if(getBalanceFactor(node->right) > 0 ) {
-		// For left child, right subtree has greater height. Hence, use RL rotation
-		node = RLRotation(node);
-	} else {
-		// For left child, left subtree has greater height. Hence, use RR rotation
-		node = RRRotation(node);
-	}
-}
-		updateHeight(node);
-		return node;
-}
-
-	void inorderRec(Node *t){
-		if(t == nullptr){
-			return;
-		}
-		inorderRec(t->left);
-		cout<< t->key << "|"<<getBalanceFactor(t)<<"|" << " ";
-		inorderRec(t->right);
-	}
-
-	Node* insertSubTree(Node *curr,int key,string value){
-		if(curr == nullptr){
-			Node *newNode = new Node(key , value);
-			return newNode;
-		}
-		if(curr->key > key){
-			curr->left = insertSubTree(curr->left , key , value);
-		}
-		else if(curr->key < key){
-			curr->right = insertSubTree(curr->right , key , value);
-		}else{
-			curr->value = value;
-			return curr;
-		}
-		return balance(curr);
-	}
-
-public:
-	void insert(int key , string value)
-	{
-		root = insertSubTree(root , key , value);
-	}
-	void inorder()
-	{
-		inorderRec(root);
-		cout<<endl;
-	}
-	Node* search(int key)
-	{
-		Node* curr = root;
-		while(curr != nullptr)
-		{
-			if(curr->key > key){
-				curr = curr->left;
-			}
-			else if(curr->key < key){
-				curr = curr->right;
-			}
-			else{
-				return curr;
-			}
-		}
-		return nullptr;
-	}
-
-	void BFS()
-	{
-		queue <Node*> q;
-		q.push(root);
-		while(!q.empty()){
-			Node* popNode = q.front();
-			cout<<popNode->key << " ";
-			q.pop();
-			if(popNode->left != nullptr){
-				q.push(popNode->left);
-			}
-			if(popNode->right != nullptr){
-				q.push(popNode->right);
-			}
-		}
-		cout<<endl;
-	}
+class avl{
+    node *root;
+    public:
+    avl(){
+        root = nullptr;
+    }
+    node *getRoot(){
+        return root;
+    }
+    int getHeight(node *t){
+        if(t == nullptr){
+            return 0;
+        }
+        return t->height;
+    }
+    int getBalanceFactor(node *t){
+        if(t == nullptr){
+            return 0;
+        }
+        return (getHeight(t->left) - getHeight(t->right));
+    }
+    void updateHeight(node *t){
+        int hl = getHeight(t->left);
+        int hr = getHeight(t->right);
+        t->height = 1 + max(hl,hr);
+    }
+    
+    node *LLRotation(node *t)
+    {
+        node *newnode = t->left ;
+        t->left = newnode->right;
+        newnode->right = t;
+        
+        updateHeight(t);
+        updateHeight(newnode);
+        
+        cout<<"[LL]"<<endl;
+        return newnode;
+    }
+    node* RRRotation(node *t)
+    {
+        node *newnode = t->right;
+        t->right = newnode->left;
+        newnode->left = t;
+        
+        updateHeight(t);
+        updateHeight(newnode);
+        
+        std::cout << "[RR]" << std::endl;
+        return newnode;
+    }
+    node* LRRotation(node *t)
+    {
+        t->left = RRRotation(t->left);
+        return LLRotation(t);
+        std::cout << "[LR]" << std::endl;
+    }
+    node* RLRotation(node *t)
+    {
+        t->right = LLRotation(t->right);
+        return RRRotation(t);
+        std::cout << "[RL]" << std::endl;
+    }
+    
+    node* balance(node *t){
+        if(getBalanceFactor(t) == 2){
+            if(getBalanceFactor(t->left) < 0){
+                std::cout << "call(lr_)" << std::endl;
+                t = LRRotation(t);
+            }else{
+                std::cout << " call ll" << std::endl;
+                t = LLRotation(t);
+            }
+        }
+        else if(getBalanceFactor(t)==-2){
+            if(getBalanceFactor(t->right)>0){
+                std::cout << "call rl" << std::endl;
+                t = RLRotation(t);
+            }else{
+                std::cout << "call rr" << std::endl;
+                t = RRRotation(t);
+            }
+        }
+        updateHeight(t);
+        return t;
+    }
+    
+    void inorder(node* t){
+        if(t == nullptr){
+            return;
+        }
+        inorder(t->left);
+        std::cout << t->key << " ";
+        inorder(t->right);
+    }
+    
+    node* insert(node* curr ,int key,string value)
+    {
+        if(curr == nullptr){
+            node *newnode = new node(key,value);
+            return newnode;
+        }
+        if(curr->key < key){
+            curr->right = insert(curr->right,key,value);
+        }else if(curr->key > key){
+            curr->left = insert(curr->left , key,value);
+        }else{
+            curr->value = value;
+            return curr;
+        }
+        return balance(curr);
+    }
+    
+    
+    void insert_data(int key,string value){
+        root = insert(root , key,value);
+    }
+    
+    void bfs()
+    {
+        queue<node*> q ;
+        node *t;
+        q.push(root);
+        while(!q.empty()){
+            t = q.front();
+            q.pop();
+            cout<<t->key << "|" << getBalanceFactor(t)<< "|"<<" ";
+            
+            if(t->left != nullptr){
+                q.push(t->left);
+            }
+            if(t->right != nullptr){
+                q.push(t->right);
+            }
+        }
+        cout<<endl;
+    }
+    
+    node* findminValnode(node*t)
+    {
+        if(t==nullptr || t->left==nullptr){
+            return t;
+        }
+        return findminValnode(t->left);
+    }
+    
+    node* deleteNode(node* curr , int key)
+    {
+        if(curr == nullptr){
+            return nullptr;
+        }
+        if(key < curr->key)
+        {
+            curr->left = deleteNode(curr->left , key);
+        }else if(key > curr->key){
+            curr->right = deleteNode(curr->right , key);
+        }else{
+            if(curr->left == nullptr || curr->right == nullptr){
+                node *temp;
+                temp = curr->left ? curr->left : curr->right;
+                if(temp == nullptr){
+                    temp = curr;
+                    curr = nullptr;
+                }else{
+                    *curr = *temp;
+                }
+                delete(temp);
+            }else{
+                node *temp;
+                temp = findminValnode(curr->right);
+                curr->key = temp->key;
+                curr->value = temp->value;
+                curr->right = deleteNode(curr->right , temp->key);
+            }
+        }
+        
+        if(curr == nullptr){
+            return nullptr;
+        }
+        return balance(curr);
+    }
+    
+    void delete_ele(int key){
+        root = deleteNode(root,key);
+    }
 };
 
-int main() {
-cout << "!!!AVL TREE!!!" << endl;
-AVLTree tree;
-int key;
-string value;
-int ch;
-Node* res;
+int main()
+{
+    avl tree;
+    tree.insert_data(20,"a");
+    tree.insert_data(10,"b");
+    tree.insert_data(30,"c");
+    tree.insert_data(15,"c");
+    tree.insert_data(11,"c");
+    tree.insert_data(12,"c");
+    tree.insert_data(14,"c");
+    //tree.inorder(tree.getRoot());
+    tree.bfs();
+    
+    tree.delete_ele(14);
+    tree.bfs();
+    
+     tree.delete_ele(11);
+    tree.bfs();
 
-while(true){
-
-	cout<<"1.Insert(key,value) 2.Inorder 3.search 4.BFS 5.Exit"<<endl;
-	cin>>ch;
-
-	switch(ch){
-	case 1:
-		cout<<"Enter key and Value : ";
-		cin>>key>>value;
-		tree.insert(key, value);
-		break;
-	case 2:
-		cout<<"Inorder Traversal : "<<endl;
-		tree.inorder();
-		break;
-	case 3:
-		cout<<"Enter key to search :";
-		cin>>key;
-		res = tree.search(key);
-		if(res == nullptr){
-			cout<<"Key not found"<<endl;
-		}else{
-			cout<<"Key found Value is :"<<res->getValue()<<endl;
-		}
-		break;
-	case 4:
-		cout<<"BFS traversal :"<<endl;
-		tree.BFS();
-		break;
-	case 5:
-		exit(0);
-	default:
-		exit(0);
-	}
+    return 0;
 }
-
-return 0;
-}
-
